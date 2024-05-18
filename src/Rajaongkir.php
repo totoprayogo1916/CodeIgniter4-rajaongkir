39ad604da0201f04100b7a05b55bbea74e37928a
totoprayogo1916 <totoprayogo1916@users.noreply.github.com>
2024-05-18 15:35:04 +0000

    Apply changes

--- src/Rajaongkir.php
+++ src/Rajaongkir.php
@@ -22,8 +22,8 @@ class Rajaongkir
 {
     // Account types
     public const ACCOUNT_STARTER = 'starter';
-    public const ACCOUNT_BASIC = 'basic';
-    public const ACCOUNT_PRO = 'pro';
+    public const ACCOUNT_BASIC   = 'basic';
+    public const ACCOUNT_PRO     = 'pro';
 
     // Account type for the current instance
     protected $accountType = self::ACCOUNT_STARTER;
@@ -47,8 +47,8 @@ class Rajaongkir
     // Supported couriers for each account type
     protected $supportedCouriers = [
         self::ACCOUNT_STARTER => ['jne', 'pos', 'tiki'],
-        self::ACCOUNT_BASIC => ['esl', 'jne', 'pcp', 'pos', 'rpx', 'tiki'],
-        self::ACCOUNT_PRO => [
+        self::ACCOUNT_BASIC   => ['esl', 'jne', 'pcp', 'pos', 'rpx', 'tiki'],
+        self::ACCOUNT_PRO     => [
             'cahaya', 'dse', 'esl', 'expedito*', 'first', 'idl', 'indah', 'j&t', 'jet', 'jne',
             'lion', 'ncs', 'ninja-express', 'pahala', 'pandu', 'pcp', 'pos', 'rex', 'rpx', 'sap',
             'sicepat', 'slis', 'star', 'tiki', 'wahana',
@@ -58,35 +58,35 @@ class Rajaongkir
     // Supported waybills for each account type
     protected $supportedWayBills = [
         self::ACCOUNT_STARTER => [],
-        self::ACCOUNT_BASIC => ['jne'],
-        self::ACCOUNT_PRO => [
+        self::ACCOUNT_BASIC   => ['jne'],
+        self::ACCOUNT_PRO     => [
             'dse', 'first', 'j&t', 'jet', 'jne', 'pcp', 'pos', 'rpx', 'sap', 'sicepat', 'tiki', 'wahana',
         ],
     ];
 
     // List of couriers and their descriptions
     protected $couriersList = [
-        'cahaya' => 'Cahaya Logistik (CAHAYA)',
-        'dse' => '21 Express (DSE)',
-        'esl' => 'Eka Sari Lorena (ESL)',
+        'cahaya'    => 'Cahaya Logistik (CAHAYA)',
+        'dse'       => '21 Express (DSE)',
+        'esl'       => 'Eka Sari Lorena (ESL)',
         'expedito*' => 'Expedito*',
-        'first' => 'First Logistics (FIRST)',
-        'indah' => 'Indah Logistic (INDAH)',
-        'j&t' => 'J&T Express (J&T)',
-        'jet' => 'JET Express (JET)',
-        'jne' => 'Jalur Nugraha Ekakurir (JNE)',
-        'ncs' => 'Nusantara Card Semesta (NCS)',
-        'pahala' => 'Pahala Kencana Express (PAHALA)',
-        'pandu' => 'Pandu Logistics (PANDU)',
-        'pcp' => 'Priority Cargo and Package (PCP)',
-        'pos' => 'POS Indonesia (POS)',
-        'rpx' => 'RPX Holding (RPX)',
-        'sap' => 'SAP Express (SAP)',
-        'sicepat' => 'SiCepat Express (SICEPAT)',
-        'slis' => 'Solusi Express (SLIS)',
-        'star' => 'Star Cargo (STAR)',
-        'tiki' => 'Citra Van Titipan Kilat (TIKI)',
-        'wahana' => 'Wahana Prestasi Logistik (WAHANA)',
+        'first'     => 'First Logistics (FIRST)',
+        'indah'     => 'Indah Logistic (INDAH)',
+        'j&t'       => 'J&T Express (J&T)',
+        'jet'       => 'JET Express (JET)',
+        'jne'       => 'Jalur Nugraha Ekakurir (JNE)',
+        'ncs'       => 'Nusantara Card Semesta (NCS)',
+        'pahala'    => 'Pahala Kencana Express (PAHALA)',
+        'pandu'     => 'Pandu Logistics (PANDU)',
+        'pcp'       => 'Priority Cargo and Package (PCP)',
+        'pos'       => 'POS Indonesia (POS)',
+        'rpx'       => 'RPX Holding (RPX)',
+        'sap'       => 'SAP Express (SAP)',
+        'sicepat'   => 'SiCepat Express (SICEPAT)',
+        'slis'      => 'Solusi Express (SLIS)',
+        'star'      => 'Star Cargo (STAR)',
+        'tiki'      => 'Citra Van Titipan Kilat (TIKI)',
+        'wahana'    => 'Wahana Prestasi Logistik (WAHANA)',
     ];
 
     /**
@@ -99,7 +99,7 @@ class Rajaongkir
     {
         if (isset($apiKey)) {
             $this->apiKey = is_array($apiKey) ? ($apiKey['api_key'] ?? null) : $apiKey;
-            $accountType = is_array($apiKey) ? ($apiKey['account_type'] ?? $accountType) : $accountType;
+            $accountType  = is_array($apiKey) ? ($apiKey['account_type'] ?? $accountType) : $accountType;
         }
 
         if (isset($accountType)) {
@@ -159,7 +159,7 @@ class Rajaongkir
         switch ($this->accountType) {
             case self::ACCOUNT_PRO:
                 $apiUrl = 'https://pro.rajaongkir.com';
-                $path = 'api/' . $path;
+                $path   = 'api/' . $path;
                 break;
 
             case self::ACCOUNT_BASIC:
@@ -173,12 +173,12 @@ class Rajaongkir
         }
 
         $client = Services::curlrequest();
-        $uri = $apiUrl . '/' . $path;
+        $uri    = $apiUrl . '/' . $path;
 
         $client->setHeader('key', $this->apiKey);
         $this->response = $client->request($type, $uri, ['query' => $params]);
 
-        $body = json_decode($this->response->getBody(), true, 512, JSON_THROW_ON_ERROR)['rajaongkir'];
+        $body   = json_decode($this->response->getBody(), true, 512, JSON_THROW_ON_ERROR)['rajaongkir'];
         $status = $body['status'];
 
         if ($status['code'] === 200) {
@@ -289,9 +289,9 @@ class Rajaongkir
     public function getCost($origin, $destination, $metrics, $courier, $originType = 'city', $destinationType = 'city')
     {
         $params = [
-            'origin' => $origin,
+            'origin'      => $origin,
             'destination' => $destination,
-            'courier' => $courier,
+            'courier'     => $courier,
         ];
 
         if ($originType === 'subdistrict') {
